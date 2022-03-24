6b18ae687896aba576b7f4a218d71cbd428a6cb2
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-24 13:03:41 -0600

    coaches show page

--- spec/features/coaches/show_spec.rb
+++ spec/features/coaches/show_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe 'coaches index page' do
-  it 'displays the list of coaches' do
+RSpec.describe 'coaches show page' do
+  it 'displays coaches and attributes' do
     coach = Coach.create!(name: 'Doc McStuffins', club: "Bruins", recruiting_athletes: true, years_experience: 20)
     coach_2 = Coach.create!(name: "Missy Franklin", club: "Dolphins", recruiting_athletes: true, years_experience: 2)
     coach_3 = Coach.create!(name: "Roger Federer", club: "Aces", recruiting_athletes: false, years_experience: 15)
 
-    visit "/coaches"
+    visit "/coaches/#{coach.id}"
 
     save_and_open_page
     expect(page).to have_content(coach.name)
+    expect(page).to have_content(coach.club)
+    expect(page).to have_content(coach.recruiting_athletes)
+    expect(page).to have_content(coach.years_experience)
   end
 end
