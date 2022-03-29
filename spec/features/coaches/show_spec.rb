7997480ad4e1a9344e4e836a0443c9f8b1a460b2
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-29 16:56:41 -0600

    test: user story two

--- spec/features/coaches/show_spec.rb
+++ spec/features/coaches/show_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 RSpec.describe 'coaches show page' do
   it 'displays coaches and attributes' do
     coach = Coach.create!(name: 'Doc McStuffins', club: "Bruins", recruiting_athletes: true, years_experience: 20)
-    coach_2 = Coach.create!(name: "Missy Franklin", club: "Dolphins", recruiting_athletes: true, years_experience: 2)
-    coach_3 = Coach.create!(name: "Roger Federer", club: "Aces", recruiting_athletes: false, years_experience: 15)
+    franklin = Coach.create!(name: "Missy Franklin", club: "Dolphins", recruiting_athletes: true, years_experience: 2)
+    federer = Coach.create!(name: "Roger Federer", club: "Aces", recruiting_athletes: false, years_experience: 15)
 
     visit "/coaches/#{coach.id}"
 
-    # save_and_open_page
+    save_and_open_page
     expect(page).to have_content(coach.name)
     expect(page).to have_content(coach.club)
     expect(page).to have_content(coach.recruiting_athletes)
