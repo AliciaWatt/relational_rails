e3643d1951a8b36add288f3b487e32fe71b19329
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-30 13:17:07 -0600

    test spec for new coach

---
+++ spec/features/coaches/new_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'New Coach' do
+
+  it 'can display a link to create a new coach on the index page' do
+
+    visit '/coaches'
+
+    click_link("Create New Coach")
+
+    expect(current_path).to eq('/coaches/new')
+  end
+  
+  it 'can create a new Coach' do
+
+  visit '/coaches/new'
+
+  fill_in('Name', with: 'Dan Brodanawitz')
+  fill_in('Club', with: 'Marlins')
+  fill_in('Years Experience', with: '7')
+  check('recruiting_athletes')
+
+  click_button('Create Coach')
+  expect(current_path).to eq('/coaches')
+  expect(page).to have_content('Dan Brodanawitz')
+  expect(page).to have_content('Marlins')
+  expect(page).to have_content('7')
+  expect(page).to have_content('false')
+  end
+end
