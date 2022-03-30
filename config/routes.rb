b2d7214e379fbeff3c71e5d8e80f0b088b30bcb9
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-30 13:18:17 -0600

    add routes for new coach and create coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/coaches', to: 'coaches#index'
+  get '/coaches/new', to: 'coaches#new'
   get '/coaches/:id', to: 'coaches#show'
   get '/coaches/:coach_id/athletes', to: 'coach_athletes#index'
   get '/athletes', to: 'athletes#index'
   get '/athletes/:id', to: 'athletes#show'
+  post '/coaches', to: 'coaches#create'
 
 end
