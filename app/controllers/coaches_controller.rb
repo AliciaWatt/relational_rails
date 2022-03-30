91558257cc27bc0bdb4cc512e97ccc49f3da640d
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-30 13:19:18 -0600

    feat: new and create for coach

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -3,7 +3,17 @@ class CoachesController < ApplicationController
     @coaches = Coach.all
   end
 
+  def new
+  end
+
+  def create
+  end
+
   def show
     @coach = Coach.find(params[:id])
+  end
+
+  def coach_params
+    params.permit(:name, :club, :years_experience, :recruiting_athletes)
   end
 end
