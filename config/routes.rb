2cfb5300e2ff927bddca77f2a81416e0cd84fd26
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-30 17:08:46 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,18 @@ Rails.application.routes.draw do
   get '/coaches/new', to: 'coaches#new'
   get '/coaches/:id', to: 'coaches#show'
   get '/coaches/:id/edit', to: 'coaches#edit'
-  get '/coaches/:coach_id/athletes', to: 'coach_athletes#index'
   get '/athletes', to: 'athletes#index'
   get '/athletes/:id', to: 'athletes#show'
+  get '/coaches/:coach_id/athletes', to: 'coach_athletes#index'
+  get '/coaches/:coach_id/athletes/new', to: 'coach_athletes#new'
+  get '/athletes/:id/edit', to: 'athletes#edit'
+  
+
   post '/coaches', to: 'coaches#create'
   patch '/coaches/:id', to: 'coaches#update'
+
+  post '/coaches/:coach_id/athletes', to: 'coach_athletes#create'
+  patch '/athletes/:id', to: 'athletes#update'
+
 
 end
