40bf4b10da9779a4530da899c1522007418d312d
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-30 17:05:39 -0600

    test athlete update

---
+++ spec/features/athletes/update_spec.rb
@@ -0,0 +1,31 @@
+require rails_helper
+
+RSpec.describe 'Updating athlete' do
+  before :each do
+    @doc = Coach.create!(name: 'Doc McStuffins', club: "Bruins", recruiting_athletes: true, years_experience: 20)
+    @stanski = Athlete.create!(name: 'Staniel Stanski', rank: 3, championship_qualifier: true, coach_id: @doc.id)
+  end
+
+  it 'can link to the update form' do
+    visit "/athletes/#{@stanski.id}"
+
+    click_link 'Update Information'
+
+    expect(current_path).to eq("/athletes/#{@stanski.id}/edit")
+  end
+
+  it 'allows for attributes to be updated' do
+
+    visit "/athletes/#{@stanski.id}/edit"
+
+    fill_in('Name', with: 'Staniel Stanski Jr.')
+    fill_in('rank', with: 2)
+    check('championship_qualifier')
+
+    click_button("Submit")
+    expect(current_path).to eq("/athletes/#{@stanski.id}")
+    expect(page).to have_content('Staniel Stanski Jr.')
+    expect(page).to have_content('2')
+    expect(page).to have_content('true')
+  end
+end
