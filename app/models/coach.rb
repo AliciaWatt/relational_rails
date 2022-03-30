34087482aaae5cb545906fcab1f8fff0f3d941c6
Alicia Watt <alicia.watt.aw@gmail.com>
2022-03-29 22:59:35 -0600

    add desc_order to coach model"
    '

--- app/models/coach.rb
+++ app/models/coach.rb
@@ -1,3 +1,8 @@
 class Coach < ApplicationRecord
   has_many :athletes
+  validates_presence_of :name, :club, :recruiting_athletes, :years_experience
+
+  def self.desc_order
+    order(created_at: :desc)
+  end
 end
